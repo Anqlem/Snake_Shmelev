1f2c905d00fe9118bde5b79b6a614620cb927472
Миша Шмелёв <cryptecker@hotmail.com>
2020-04-28 14:05:10 +0300

    Add project files.

---
+++ Snake_Shmelev/Program.cs
@@ -0,0 +1,86 @@
+﻿using AxWMPLib;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WMPLib;
+
+namespace Snake_Shmelev
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.SetWindowSize(80, 25);
+
+			Walls walls = new Walls(80, 25);
+			walls.Draw();
+
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
+
+			FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
+			int points = 0;
+
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					points++;
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+		}
+
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("============================", xOffset, yOffset++);
+			WriteText("  G  A  M  E   O  V  E  R", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("  Author : Mikhail Shmelev", xOffset + 2, yOffset++);
+			yOffset++;
+			WriteText("============================", xOffset, yOffset++);
+			yOffset++;
+			WriteText($" Your Score: {point}", xOffset, yOffset++);
+
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+	}
+}
